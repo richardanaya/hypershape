ed76e1c94e9ad46c08e77e16a1560418bf8fb523
Richard Anaya <richard.anaya@gmail.com>
2022-10-31 17:45:49 -0700

    basic setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.ts',
+      // Could also be a dictionary or array of multiple entry points
+      entry: 'src/main.ts',
+      name: 'VRMX',
+      // the proper extensions will be added
+      fileName: 'vrmx',
       formats: ['es']
     },
     rollupOptions: {
-      external: /^lit/
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: /^lit/,
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          lit: 'Lit'
+        }
+      }
     }
   }
 })
