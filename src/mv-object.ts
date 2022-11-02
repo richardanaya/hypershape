1f9e7e2d0b0f53c7703b84f6661b620e56680abc
Richard Anaya <richard.anaya@gmail.com>
2022-11-01 22:49:51 -0700

    basics

--- src/mv-object.ts
+++ src/mv-object.ts
@@ -1,8 +1,8 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
-import litLogo from './assets/lit.svg'
-import { MetaverseSpace } from './mv-space';
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { BoxGeometry, Euler, Mesh, MeshBasicMaterial, Object3D } from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { MetaverseSpace } from "./mv-space";
 
 /**
  * An example element.
@@ -10,8 +10,37 @@ import { MetaverseSpace } from './mv-space';
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('mv-object')
+@customElement("mv-object")
 export class MetaverseObject extends LitElement {
+  @property({ type: String, attribute: "src" })
+  src = "";
+
+  @property({ type: Number, attribute: "x" })
+  x = 0;
+
+  @property({ type: Number, attribute: "y" })
+  y = 0;
+
+  @property({ type: Number, attribute: "z" })
+  z = 0;
+
+  @property({ type: Number, attribute: "rx" })
+  rx = 0;
+
+  @property({ type: Number, attribute: "ry" })
+  ry = 0;
+
+  @property({ type: Number, attribute: "rz" })
+  rz = 0;
+
+  @property({ type: Number, attribute: "sx" })
+  sx = 1;
+
+  @property({ type: Number, attribute: "sy" })
+  sy = 1;
+
+  @property({ type: Number, attribute: "sz" })
+  sz = 1;
 
   // connected
   connectedCallback() {
@@ -19,44 +48,55 @@ export class MetaverseObject extends LitElement {
 
     // get parent element
     const parent = this.parentElement;
-    if(parent === null) {
+    if (parent === null) {
       console.error("mv-object must be a child of mv-space");
       return;
     }
-    if(parent.tagName !== "MV-SPACE") {
+    if (parent.tagName !== "MV-SPACE") {
       console.error("mv-object must be a child of mv-space");
       return;
     }
     const space = (parent as MetaverseSpace).space;
-    if(space === undefined) {
+    if (space === undefined) {
       console.error("mv-space must be defined");
       return;
     }
 
-    // add random cubes with random color
-    const material = new MeshBasicMaterial({ color: Math.random() * 0xffffff })
-      
-    for (let i = 0; i < 100; i++) {
-      const geometry = new BoxGeometry()
-      const cube = new Mesh(geometry, material)
-      cube.position.x = Math.random() * 10 - 5
-      cube.position.y = Math.random() * 10 - 5
-      cube.position.z = Math.random() * 10 - 5
-      space.add(cube)
+    const root = new Object3D();
+
+    const material = new MeshBasicMaterial({ color: Math.random() * 0xffffff });
+
+    if (this.src === "") {
+      const geometry = new BoxGeometry();
+      const cube = new Mesh(geometry, material);
+      root.add(cube);
+    } else {
+      const loader = new GLTFLoader()
+      loader.load(this.src, (gltf) => {
+        root.add(gltf.scene);
+      });
     }
+
+    root.position.x = this.x;
+    root.position.y = this.y;
+    root.position.z = this.z;
+    root.rotation.x = this.rx;
+    root.rotation.y = this.ry;
+    root.rotation.z = this.rz;
+    root.scale.x = this.sx;
+    root.scale.y = this.sy;
+    root.scale.z = this.sz;
+    space.add(root);
   }
 
   render() {
-    console.log("object")
-    return html`
-      
-    `
+    console.log("object");
+    return html``;
   }
-
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'mv-object': MetaverseObject
+    "mv-object": MetaverseObject;
   }
 }
--- src/world.ts
+++ src/world.ts
@@ -1,4 +1,4 @@
-import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+import {ACESFilmicToneMapping, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, PCFSoftShadowMap, PerspectiveCamera, Scene, sRGBEncoding, WebGLRenderer} from "three";
 
 let scene!: Scene;
 
@@ -17,7 +17,31 @@ export function getWorld() {
     if(scene === undefined) {
         // create threejs renderer and scene and animation loop
         const renderer = new WebGLRenderer({ canvas: canvas })
+        // physical based rendering
+        renderer.physicallyCorrectLights = true
+        renderer.outputEncoding = sRGBEncoding
+        renderer.toneMapping = ACESFilmicToneMapping
+        renderer.toneMappingExposure = 1.0
+        renderer.shadowMap.enabled = true
+        renderer.shadowMap.type = PCFSoftShadowMap
+
         scene = new Scene()
+
+        // add global illumination
+
+        
+        // add direction light
+
+        const light = new DirectionalLight(0xffffff, 10.0)
+        light.position.set(0, 1, 0)
+        light.castShadow = true
+        light.shadow.mapSize.width = 2048
+        light.shadow.mapSize.height = 2048
+        light.shadow.camera.near = 0.5
+        light.shadow.camera.far = 500
+        scene.add(light)
+
+
         const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
         camera.position.z = 5
         
