dd682abbab0d844e6ad12bc736a2ebeb67b06ca6
Richard Anaya <richard.anaya@gmail.com>
2022-11-13 20:34:27 -0800

    rename

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: 'src/main.ts',
-      name: 'hyperspace',
+      name: 'hypershape',
       // the proper extensions will be added
-      fileName: 'hyperspace',
+      fileName: 'hypershape',
       formats: ['es']
     }
   }
