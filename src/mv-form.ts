772f6e486c81591d5aa0d7e77fcddb79a9c20e74
Richard Anaya <richard.anaya@gmail.com>
2023-08-09 11:24:40 -0700

    adding processor

--- src/mv-form.ts
+++ src/mv-form.ts
@@ -11,6 +11,9 @@ export class MetaverseForm extends LitElement {
   @property({ type: String, attribute: "method" })
   method = "";
 
+  @property({ type: String, attribute: "target" })
+  target = "";
+
   inputs: MetaverseInput[] = [];
 
   createRenderRoot() {
@@ -30,28 +33,46 @@ export class MetaverseForm extends LitElement {
     this.inputs.push(input);
   }
 
-  submit() {
+  async submit() {
     const nameToValue = this.inputs.reduce((acc, input) => {
       if (input.name === "") return acc;
       acc[input.name] = input.value;
       return acc;
     }, {} as any);
     getWorld().addWorldInfoToForm(nameToValue);
-    // create a new invisible form and submit it
-    const form = document.createElement("form");
-    form.style.display = "none";
-    form.method = this.method;
-    form.action = this.action;
-    for (const name in nameToValue) {
-      const value = nameToValue[name];
-      const input = document.createElement("input");
-      input.type = "hidden";
-      input.name = name;
-      input.value = value;
-      form.appendChild(input);
+    if (this.target === "") {
+      // create a new invisible form and submit it
+      const form = document.createElement("form");
+      form.style.display = "none";
+      form.method = this.method;
+      form.action = this.action;
+      for (const name in nameToValue) {
+        const value = nameToValue[name];
+        const input = document.createElement("input");
+        input.type = "hidden";
+        input.name = name;
+        input.value = value;
+        form.appendChild(input);
+      }
+      document.body.appendChild(form);
+      form.submit();
+    } else {
+      const formData = new FormData();
+      for (const name in nameToValue) {
+        const value = nameToValue[name];
+        formData.append(name, value);
+      }
+
+      const r = await fetch(this.action, {
+        method: this.method,
+        body: formData,
+      });
+      const text = await r.text();
+      const target = document.body.querySelector("#" + this.target);
+      if (target) {
+        target.innerHTML = text;
+      }
     }
-    document.body.appendChild(form);
-    form.submit();
   }
 }
 
